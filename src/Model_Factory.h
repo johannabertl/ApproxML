d7c6ad242a0904ce6a643a6ae7019733fbc58692
johannabertl <johannabertl@fastmail.fm>
2019-01-25 10:12:12 +0100

    cleaned up header files, put function declaration of Creat_Models into the Model_Factory class definition, and made testmodelfac work properly.

--- src/Model_Factory.h
+++ src/Model_Factory.h
@@ -1,14 +1,20 @@
 #ifndef MODEL_FACTORY_H
 #define MODEL_FACTORY_H
 
-#include "Model.h"
 #include <RcppArmadillo.h>
+#include <Model.h>
+#include <Normal.h>
 
 using namespace arma;
 
+
 class Model_Factory {
 public:
-  Model* Create_Model(String type);
+  Model* Create_Model(String type) {
+    if ( type == "Normal" ) return new Normal();
+    //  if ( type == "square" ) return new Square();
+    return NULL;
+  }
 };
 
 #endif 
--- src/testmodelfac.cpp
+++ src/testmodelfac.cpp
@@ -1,6 +1,5 @@
 #include <RcppArmadillo.h>
 #include "Model_Factory.h"
-#include "Create_Model.h"
 #include "Model.h"
 #include "Normal.h"
 
@@ -10,16 +9,16 @@ using namespace arma;
 // [[Rcpp::depends(RcppArmadillo)]]
 
 // [[Rcpp::export]]
-arma::vec testmodelfac(String type, arma::vec parameters, arma::mat fixed_parameters)
+arma::mat testmodelfac(String type, arma::vec parameters, arma::mat fixed_parameters)
 {
   Model_Factory* myFactory = new Model_Factory();
-  //Model* std_norm = myFactory->Create_Model(type);
+  Model* std_norm = myFactory->Create_Model(type);
 
-  //std_norm->parameters = parameters;
-  //std_norm->fixed_parameters = fixed_parameters;
-  //arma::mat simres = std_norm->simulate(10);
-  //return simres;
-  return parameters;
+  std_norm->parameters = parameters;
+  std_norm->fixed_parameters = fixed_parameters;
+  arma::mat simres = std_norm->simulate(10);
+  return simres;
+  //return parameters;
 }
 
 
